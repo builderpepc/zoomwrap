f2b0ee388b0b5f0fad6cd0f8c6e0ac4b21e7c207
builderpepc <troy.hiruka@gmail.com>
2021-04-21 16:09:07 -0400

    set up pypi

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'zoomwrap',
+  packages = ['zoomwrap'],
+  version = '0.1',
+  license='MIT',
+  description = 'A module containing classes that can be easily serialized to JSON for use with Zoom\'s APIs.',   # Give a short description about your library
+  author = 'builderpepc',
+  author_email = 'troy.hiruka@gmail.com',
+  url = 'https://github.com/builderpepc/zoomwrap',
+  download_url = 'hhttps://github.com/builderpepc/zoomwrap/archive/v0.1.tar.gz',
+  keywords = ['zoom', 'zoom.us', 'zoom-api', 'bot'],
+  install_requires=[
+          'requests',
+          'ujson', # Faster json
+          'regex'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
