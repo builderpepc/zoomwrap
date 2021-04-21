edd362ab43889883a30c6f7a0fd03cb45e6abb04
builderpepc <troy.hiruka@gmail.com>
2021-04-21 17:14:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
   packages = ['zoomwrap'],
   version = '0.1.1',
   license='MIT',
-  description = 'A module containing classes that can be easily serialized to JSON for use with Zoom\'s APIs.',   # Give a short description about your library
+  description = 'A module containing classes that can be easily serialized to JSON for use with Zoom\'s chat APIs.',   # Give a short description about your library
   author = 'builderpepc',
   author_email = 'troy.hiruka@gmail.com',
   url = 'https://github.com/builderpepc/zoomwrap',
-  download_url = 'https://github.com/builderpepc/zoomwrap/archive/v0.1.tar.gz',
+  download_url = 'https://github.com/builderpepc/zoomwrap/archive/v0.1.1.tar.gz',
   keywords = ['zoom', 'zoom.us', 'zoom-api', 'bot'],
   install_requires=[
           'requests',
